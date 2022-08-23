16d8aa350e9d6539706a40751b6761e6ca0977c5
Jason <eritpchy@qq.com>
2022-08-23 18:24:16 +0800

    - remove implementation org.slf4j:slf4j-android:1.7.32

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,6 @@ dependencies {
     implementation ("com.fasterxml.jackson.core:jackson-core:2.11.3")
     implementation ("com.fasterxml.jackson.core:jackson-databind:2.11.3")
     implementation ("org.slf4j:slf4j-api:1.7.32")
-    implementation ("org.slf4j:slf4j-android:1.7.32")
     compileOnly ("org.apache.tomcat.embed:tomcat-embed-core:10.0.21")
 //    compileOnly ("org.eclipse.jetty:jetty-webapp:8.0.0.v20110901")
 //    compileOnly ("org.mortbay.jetty:servlet-api:3.0.20100224")
